0a6b4d193d4c296e42a089a1a72b16f97f4c3abe
zeerak ali <mrzeerakalikhan@gmail.com>
2019-12-08 18:55:29 +0500

    arrays

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+let cricket_team_names=["pakistan" ,"england" ,"australia" ,"newzealand" ,"scotland"];
+let world_cup_winning_year=["1992" ,"1986" ,"2001" ,"1982" ,"nill"];
+print!("Cricket Team:{}",cricket_team_names[0] );//,"Year:{}",world_cup_winning_year[0]);
+print!("-----");
+print!("Year:{}",world_cup_winning_year[0] );//,"Year:{}",world_cup_winning_year[0]);
+println!("\n");
+
+print!("Cricket Team:{}",cricket_team_names[1] );//,"Year:{}",world_cup_winning_year[0]);
+print!("-----");
+print!("Year:{}",world_cup_winning_year[1] );//,"Year:{}",world_cup_winning_year[0]);
+println!("\n");
+
+
+print!("Cricket Team:{}",cricket_team_names[2] );//,"Year:{}",world_cup_winning_year[0]);
+print!("-----");
+print!("Year:{}",world_cup_winning_year[2] );//,"Year:{}",world_cup_winning_year[0]);
+println!("\n");
+
+print!("Cricket Team:{}",cricket_team_names[3] );//,"Year:{}",world_cup_winning_year[0]);
+print!("-----");
+print!("Year:{}",world_cup_winning_year[3] );//,"Year:{}",world_cup_winning_year[0]);
+println!("\n");
+
+print!("Cricket Team:{}",cricket_team_names[4] );//,"Year:{}",world_cup_winning_year[0]);
+print!("-----");
+print!("Year:{}",world_cup_winning_year[4] );//,"Year:{}",world_cup_winning_year[0]);
+println!("\n");
+
+}
